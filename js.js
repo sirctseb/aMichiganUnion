a14f960c326d25dc46c35c406c722bcd1d910cbc
Christopher Best <bestchris@gmail.com>
2011-07-12 02:20:53 +0000

    git-svn-id: http://sircbuntu.local/svn/kc.com@273 e287d1ed-c425-dd4e-94ca-1c9bc6275cfe

--- js.js
+++ js.js
@@ -10,6 +10,7 @@ function init() {
   $("#tier4").cake({level:4});
   $("#tier5").cake({level:5});
   $("#tier6").cake({level:6});
+  $("#tier7").cake({level:7});
   //$("#tier1").click(openpics);
   $(".tiercontent").click(function(event) {event.stopPropagation();});
 }
