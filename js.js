67d49f3ff27ae999416136c2b60af44830c287ca
Christopher Best <bestchris@gmail.com>
2012-01-05 12:07:21 -0500

    did image changing for accept and regret checkboxes in js because internet explorer is terrible

--- js.js
+++ js.js
@@ -22,6 +22,25 @@ if(debug) {
 	domain = "amichiganunion.appspot.com";
 }
 
+function regretClick() {
+	// sanity check
+	if($("#regretcheck").attr("checked")) {
+		$("#regretcheck + label").css({'background-image': "url(../images/regretsChecked.png)"});
+		$("#acceptcheck + label").css({'background-image': "url(../images/acceptsUnchecked.png)"});
+	} else {
+		console.log("why not checked?");
+	}
+}
+function acceptClick() {
+	// sanity check
+	if($("#acceptcheck").attr("checked")) {
+		$("#regretcheck + label").css({'background-image': "url(../images/regretsUnchecked.png)"});
+		$("#acceptcheck + label").css({'background-image': "url(../images/acceptsChecked.png)"});
+	} else {
+		console.log("why not checked?");
+	}
+}
+
 function init() {
 	$("#slider").nivoSlider({
 		manualAdvance : true,
